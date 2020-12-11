75b43f483dd7b55b2b9beb9434b57ac3d698250a
v61265 <v61265@gmail.com>
2020-12-12 04:41:31 +0800

    Updates

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import ReactDOM from "react-dom";
-
-ReactDOM.render(<App />, document.getElementById("root"));
-
-reportWebVitals();
